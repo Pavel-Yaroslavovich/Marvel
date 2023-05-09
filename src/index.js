dd6cc7db4ada556197356040be2305aade5cf0dd
Pavel Yaroslavovich <pavel.yaroslavovich22@gmail.com>
2023-05-09 16:10:40 +0500

    marvel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, hashHistory } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <hashHistory>
-    <RouterProvider router={router} />
-  </hashHistory>
-);
+root.render(<RouterProvider router={router} />);
